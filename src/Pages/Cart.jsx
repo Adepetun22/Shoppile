398a378ae824729e456c29df7706bd78d62fe89b
Adepetun22 <adepetun22@gmail.com>
2026-02-08 13:24:16 +0100

    Update

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCart } from '../CartContext';
 import frame0 from '../assets/frame0.svg';
-import frame4 from '../assets/frame4.svg';
-import frame5 from '../assets/frame5.svg';
 
 const Cart = () => {
   const { cartItems, updateQuantity, removeFromCart } = useCart();
@@ -105,16 +103,20 @@ const Cart = () => {
                     <div className="flex items-center gap-2">
                       <button
                         onClick={() => handleQuantityChange(item.id, -1)}
-                        className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center hover:border-black hover:bg-black hover:text-white transition-all cursor-pointer"
+                        className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center hover:border-black hover:bg-black transition-all cursor-pointer group"
                       >
-                        <img src={frame4} alt="Decrease" className="w-4 h-4" />
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:fill-black">
+                          <path d="M9.46937 13.5319L4.46937 8.5319C4.39969 8.46222 4.34424 8.37943 4.30649 8.28827C4.26873 8.1971 4.2493 8.09936 4.2493 8.00065C4.2493 7.90194 4.26873 7.8042 4.30649 7.71304C4.34424 7.62187 4.39969 7.53908 4.46937 7.4694L9.46937 2.4694C9.61027 2.32851 9.80137 2.24935 10.0006 2.24935C10.1999 2.24935 10.391 2.32851 10.5319 2.4694C10.6728 2.6103 10.7519 2.8014 10.7519 3.00065C10.7519 3.19991 10.6728 3.391 10.5319 3.5319L6.06254 8.00065L10.5325 12.4694C10.6734 12.6103 10.7526 12.8014 10.7526 13.0007C10.7526 13.1999 10.6734 13.391 10.5325 13.5319C10.3916 13.6728 10.2005 13.752 10.0013 13.752C9.80202 13.752 9.61093 13.6728 9.47003 13.5319L9.46937 13.5319Z" fill="#6B7280"/>
+                        </svg>
                       </button>
                       <span className="w-8 text-center font-medium">{item.quantity}</span>
                       <button
                         onClick={() => handleQuantityChange(item.id, 1)}
-                        className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center hover:border-black hover:bg-black hover:text-white transition-all cursor-pointer"
+                        className="w-8 h-8 rounded-full border border-gray-300 flex items-center justify-center hover:border-black hover:bg-black transition-all cursor-pointer group"
                       >
-                        <img src={frame5} alt="Increase" className="w-4 h-4" />
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:fill-black">
+                          <path d="M6.53061 2.4694L11.5306 7.4694C11.6005 7.53908 11.656 7.62187 11.6939 7.71304C11.7317 7.8042 11.7512 7.90194 11.7512 8.00065C11.7512 8.09936 11.7317 8.1971 11.6939 8.28827C11.656 8.37943 11.6005 8.46222 11.5306 8.5319L6.53061 13.5319C6.38972 13.6728 6.19862 13.752 5.99936 13.752C5.80011 13.752 5.60901 13.6728 5.46811 13.5319C5.32722 13.391 5.24806 13.1999 5.24806 13.0007C5.24806 12.8014 5.32722 12.6103 5.46811 12.4694L9.93749 8.00003L5.46749 3.53065C5.32659 3.38976 5.24744 3.19866 5.24744 2.9994C5.24744 2.80015 5.32659 2.60905 5.46749 2.46815C5.60838 2.32726 5.79948 2.2481 5.99874 2.2481C6.198 2.2481 6.38909 2.32726 6.52999 2.46815L6.53061 2.4694Z" fill="#6B7280"/>
+                        </svg>
                       </button>
                     </div>
                     <button
